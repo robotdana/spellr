7445d965c5f1a8e48b200acc86ecb679139b491b
Dana Sherson <robot@dana.sh>
2019-03-22 12:26:50 +1100

    Multiple dictionaries

--- lib/spellr.rb
+++ lib/spellr.rb
@@ -1,7 +1,9 @@
 require_relative "spellr/version"
-require_relative "spellr/files"
 require_relative "spellr/token"
 require_relative "spellr/line"
+require_relative "spellr/files"
+require_relative "spellr/dictionary"
+require_relative "spellr/dictionary_loader"
 
 module Spellr
   class Error < StandardError; end
--- lib/spellr/dictionary.rb
+++ lib/spellr/dictionary.rb
@@ -1,34 +1,39 @@
 require 'pathname'
-require 'net/http'
 
 module Spellr
   class Dictionary
-    DATA_PATH = Pathname.new(__FILE__).join('..', '..', '..', 'data')
-    def self.download
-      uri = URI.parse('http://app.aspell.net/create')
-      uri.query = URI.encode_www_form(
-        max_size: 50,
-        spelling: %w{US AU},
-        max_variant: 0,
-        diacritic: :both,
-        special: :hacker,
-        download: :wordlist,
-        encoding: 'utf-8',
-        format: :inline
-      )
+    DIR = Pathname.new(__FILE__).join('..', '..', '..', 'dictionaries')
 
-      license, dictionary = Net::HTTP.get_response(uri).body.split('---', 2)
-      DATA_PATH.join('DICTIONARY').write(preprocess_entries(dictionary))
-      DATA_PATH.join('LICENSE').write(license)
+    include Enumerable
+
+    def self.[](name)
+      @registry ||= {}
+      @registry[name] ||= new(name)
     end
 
-    def self.preprocess_entries(dictionary)
-      dictionary.split("\n").map do |line|
-        line.downcase! # only compare lowercase. It'll be easier
-        line.sub!(/'s$/, '') # why
-        next unless line.length > 2 # why 2 letter words. You are unnecessary
-        line
-      end.compact.sort.uniq.join("\n")
+    def initialize(name)
+      @name = name
+    end
+
+    def file
+      DIR.join("#{name}.txt")
+    end
+
+    def license_file
+      DIR.join("#{name}.LICENSE.txt")
+    end
+
+    def each(&block)
+      enumerator.rewind
+      enumerator.each(&block)
+    end
+
+    private
+
+    attr_reader :name
+
+    def enumerator
+      @enumerator ||= file.each_line.lazy
     end
   end
 end
