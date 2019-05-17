a4e1a118d9416a685f48281b140289d6c217006d
Dana Sherson <robot@dana.sh>
2019-05-17 12:29:38 +1000

    Add ruby wordlist generation to the CLI

--- lib/spellr/cli.rb
+++ lib/spellr/cli.rb
@@ -1,47 +1,21 @@
 # frozen_string_literal: true
 
 require 'optparse'
+require 'pathname'
+
 require_relative '../spellr'
 
 module Spellr
   class CLI
-    def initialize # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
-      OptionParser.new do |opts| # rubocop:disable Metrics/BlockLength
-        opts.on('-w', '--wordlist', 'Outputs errors in wordlist format') do
-          require_relative 'wordlist_reporter'
-          Spellr.config.reporter = Spellr::WordlistReporter.new
-        end
+    attr_writer :fetch_output_dir
 
-        opts.on('-q', '--quiet', 'Silences all output') do
-          Spellr.config.quiet = true
-          Spellr.config.reporter = ->(_) {}
-        end
+    def initialize
+      parse_options
 
-        opts.on('-i', '--interactive', 'Runs the spell check interactively') do
-          require_relative 'interactive'
-          Spellr.config.reporter = Spellr::Interactive.new
-        end
-        opts.on('-c', '--config FILENAME', String, 'Path to the config file') do |file|
-          Spellr.config.config_file = file
-        end
-        opts.on_tail('-l', '--list', 'List files to be spellchecked') do
-          files.each { |f| puts f.relative_path_from(Pathname.pwd) }
+      check
+    end
 
-          exit
-        end
-        opts.on_tail('-v', '--version', 'Returns the current version') do
-          require_relative 'version'
-          puts(Spellr::VERSION)
-
-          exit
-        end
-        opts.on_tail('-h', '--help', 'Shows this message') do
-          puts(opts)
-
-          exit
-        end
-      end.parse!
-
+    def check
       require_relative 'check'
       checker = Spellr::Check.new(files: files)
       checker.check
@@ -52,6 +26,112 @@ module Spellr
     def files
       require_relative 'file_list'
       Spellr::FileList.new(*ARGV)
+    end
+
+    def wordlist_option(_)
+      require_relative 'wordlist_reporter'
+      Spellr.config.reporter = Spellr::WordlistReporter.new
+    end
+
+    def quiet_option(_)
+      Spellr.config.quiet = true
+      Spellr.config.reporter = ->(_) {}
+    end
+
+    def interactive_option(_)
+      require_relative 'interactive'
+      Spellr.config.reporter = Spellr::Interactive.new
+    end
+
+    def config_option(file)
+      Spellr.config.config_file = Pathname.pwd.join(file).expand_path
+    end
+
+    def dry_run_option(_)
+      files.each { |f| puts f.relative_path_from(Pathname.pwd) }
+
+      exit
+    end
+
+    def version_option(_)
+      require_relative 'version'
+      puts(Spellr::VERSION)
+
+      exit
+    end
+
+    def get_wordlist_option(command)
+      get_wordlist_dir.join(command)
+    end
+
+    def fetch_output_dir
+      @fetch_output_dir ||= Pathname.pwd.join('.spellr_wordlists/generated').expand_path
+    end
+
+    def fetch_option(wordlist) # rubocop:disable Metrics/AbcSize
+      command = fetch_wordlist_dir.join(wordlist).to_s
+      fetch_output_dir.mkpath
+
+      require 'shellwords'
+      words = `#{ARGV.to_a.unshift(command).shelljoin}`
+
+      puts fetch_output_dir.join(wordlist)
+
+      require_relative '../../lib/spellr/wordlist'
+      Spellr::Wordlist.new(fetch_output_dir.join("#{wordlist}.txt")).clean(words)
+
+      exit
+    end
+
+    def global_option(_)
+      self.fetch_output_dir = Pathname.new('~/.spellr_wordlists/generated').expand_path
+    end
+
+    def output_option(dir)
+      self.fetch_output_dir = Pathname.pwd.join(dir).expand_path
+    end
+
+    def wordlists
+      fetch_wordlist_dir.children.map { |p| p.basename.to_s }
+    end
+
+    def fetch_wordlist_dir
+      @fetch_wordlist_dir ||= Pathname.new(__dir__).parent.parent.join('bin', 'fetch_wordlist').expand_path
+    end
+
+    def parse_options # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+      OptionParser.new do |opts| # rubocop:disable Metrics/BlockLength
+        opts.banner = 'Usage: spellr [options] [files]'
+        opts.separator('')
+        opts.separator('Formatting options:')
+        opts.on('-w', '--wordlist', 'Outputs errors in wordlist format', &method(:wordlist_option))
+        opts.on('-q', '--quiet', 'Silences output', &method(:quiet_option))
+        opts.on('-i', '--interactive', 'Runs the spell check interactively', &method(:interactive_option))
+
+        opts.on('-d', '--dry-run', 'List files to be spellchecked', &method(:dry_run_option))
+        opts.separator('')
+
+        opts.separator('Wordlist options:')
+        opts.on('-g', '--global', 'Outputs the fetched wordlist to ~/.spellr_wordlists/', &method(:global_option))
+        opts.on('-o', '--output=OUTPUT', 'Outputs the fetched wordlist to OUTPUT/WORDLIST.txt', &method(:output_option))
+        opts.on('-f', '--fetch=WORDLIST', String, wordlists, <<~HELP, &method(:fetch_option))
+          Generates/fetches WORDLIST, passing all further options to the fetch script
+                                               Available wordlists: #{wordlists.join(', ')}
+                                               run `spellr --fetch WORDLIST --help` for options
+        HELP
+        opts.separator('')
+
+        opts.separator('Basic options:')
+        opts.on('-c', '--config FILENAME', String, <<~HELP, &method(:config_option))
+          Path to the config file (default ./.spellr.yml)
+        HELP
+        opts.on('-v', '--version', 'Returns the current version', &method(:version_option))
+        opts.on('-h', '--help', 'Shows this message') do
+          puts opts.help
+
+          exit
+        end
+      end.parse!
     end
   end
 end
