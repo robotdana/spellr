40cadd28845b57d4de0205b69c7aa00841e593ca
Dana Sherson <robot@dana.sh>
2020-02-23 02:49:28 +1100

    Run bin/generate/ruby on 2.7

--- bin/generate/util/write.rb
+++ bin/generate/util/write.rb
@@ -13,4 +13,9 @@ module Write
   def write_wordlist(words, name)
     Spellr::Wordlist.new(wordlist_path(name)).clean(StringIO.new(words.force_encoding('UTF-8')))
   end
+
+  def append_wordlist(words, name)
+    old_words = wordlist_path(name).read
+    write_wordlist("#{words}\n#{old_words}", name)
+  end
 end
